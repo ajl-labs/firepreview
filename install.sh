7aa9947f60ba4adf4ca64210f977d5dff6b0a58f
AJL <luc.bayo@gmail.com>
2026-07-07 17:48:50 +0200

    chore: update install script

--- install.sh
+++ install.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
-set -e  # stop on first error
+set -e
 
 make build-linux
 
-mkdir -p ~/.local/bin ~/.local/share/icons
+mkdir -p ~/.local/bin ~/.local/share/icons ~/.local/share/applications
 
-cp build/bin/FirePreview ~/.local/bin/
-cp build/appicon.png ~/.local/share/icons/firepreview.png
+install -m 755 build/bin/FirePreview ~/.local/bin/FirePreview
+install -m 644 build/appicon.png ~/.local/share/icons/firepreview.png
 
-echo "firepreview installed/updated"
+cat > ~/.local/share/applications/firepreview.desktop <<EOF
+[Desktop Entry]
+Name=FirePreview
+Exec=$HOME/.local/bin/FirePreview
+Icon=firepreview
+Type=Application
+Categories=Graphics;Utility;
+Terminal=false
+EOF
+
+echo "FirePreview installed/updated"
